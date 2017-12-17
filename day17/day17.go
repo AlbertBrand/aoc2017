9e24fcba3c83e727aa56d8df43d2e79ed254468a
Albert Brand <abrand@xebia.com>
2017-12-17 14:32:29 +0100

    day 17 optimization attempts

--- day17/day17.go
+++ day17/day17.go
@@ -13,13 +13,31 @@ func solver(steps int) {
 	println(state[insertpos+1])
 }
 
-func solver2(steps int) {
-	insertpos := 0
-	after := 0
-	for i := 1; i < 50000000; i++ {
+func solver2(steps uint32) {
+	var insertpos uint32
+	var after uint32
+	for i := uint32(1); i < 50000000; i++ {
 		insertpos = ((insertpos + steps) % i) + 1
 		if insertpos == 1 {
 			after = i
+		}
+	}
+	println(after)
+}
+
+func solver3(steps uint32) {
+	var insertpos uint32
+	var after uint32
+	var i uint32
+	for {
+		fits := (i - insertpos) / steps
+		i += fits + 1
+		insertpos = (insertpos+(fits+1)*(steps+1)-1)%i + 1
+		if insertpos == 1 {
+			after = i
+		}
+		if i >= 50000000 {
+			break
 		}
 	}
 	println(after)
@@ -34,5 +52,5 @@ func SolveFirst() {
 }
 
 func SolveSecond() {
-	solver2(335)
+	solver3(335)
 }
